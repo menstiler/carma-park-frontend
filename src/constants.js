cc54fcb40df8600949758ccde52383b90b18f89d
Mendel Stillerman <menstiler@gmail.com>
2022-12-28 00:26:32 -0500

    updating api url

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
-export const NODE_ENV = 'development'
+export const NODE_ENV = 'production'
 
 console.log(`we are in ${NODE_ENV} mode!`);
-export const API = (NODE_ENV === 'production') ? 'https://guarded-fjord-26437.herokuapp.com/' : 'http://localhost:3005/';
-export const API_WS_ROOT = (NODE_ENV === 'production') ? 'wss://guarded-fjord-26437.herokuapp.com/cable' : 'ws://localhost:3005/cable';
+export const API = (NODE_ENV === 'production') ? 'https://carmapark.herokuapp.com/' : 'http://localhost:3005/';
+export const API_WS_ROOT = (NODE_ENV === 'production') ? 'wss://carmapark.herokuapp.com/cable' : 'ws://localhost:3005/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
